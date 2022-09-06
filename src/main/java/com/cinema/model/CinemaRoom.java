bbd6b7faee43c5ae0492168aede452bd91c65a1a
Денис Векерик <den.vekeryk@gmail.com>
2022-09-06 10:48:41 +0300

    Refactored to property value.

--- src/main/java/com/cinema/model/CinemaRoom.java
+++ src/main/java/com/cinema/model/CinemaRoom.java
@@ -2,20 +2,25 @@ package com.cinema.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.*;
 
 @Getter
 @Component
 public class CinemaRoom {
-    private final int totalRows = 9;
-    private final int totalColumns = 9;
+    @Value("${cinema-room-rows}")
+    private int totalRows;
+    @Value("${cinema-room-columns}")
+    private int totalColumns;
     private final Set<Seat> availableSeats = new TreeSet<>();
     @JsonIgnore
     private final Map<UUID, Seat> purchasedSeats = new HashMap<>();
 
-    public CinemaRoom() {
+    @PostConstruct
+    private void init() {
         for (int row = 1; row < totalRows + 1; row++) {
             for (int column = 1; column < totalRows + 1; column++) {
                 availableSeats.add(new Seat(row, column));
