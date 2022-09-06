003514174d99b5e8bde41da035b6bf13420cf4f0
Денис Векерик <den.vekeryk@gmail.com>
2022-09-06 10:58:24 +0300

    Implemented POST /stats endpoint accessible by password that return the movie theatre statistics.

--- src/main/java/com/cinema/controller/CinemaRoomController.java
+++ src/main/java/com/cinema/controller/CinemaRoomController.java
@@ -2,11 +2,15 @@ package com.cinema.controller;
 
 import com.cinema.dto.PurchasedTicket;
 import com.cinema.dto.ReturnedTicket;
+import com.cinema.dto.SeatDTO;
 import com.cinema.dto.Token;
+import com.cinema.exception.custom.InvalidPasswordException;
 import com.cinema.model.CinemaRoom;
 import com.cinema.model.Seat;
+import com.cinema.model.Statistic;
 import com.cinema.service.BookingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -15,8 +19,10 @@ import java.util.UUID;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class CinemaRoomController {
-
+    @Value("${secret}")
+    private String secret;
     private final BookingService bookingService;
+    private final Statistic statistic;
 
     @GetMapping("/seats")
     public CinemaRoom seats() {
@@ -24,14 +30,23 @@ public class CinemaRoomController {
     }
 
     @PostMapping("/purchase")
-    public PurchasedTicket purchase(@RequestBody Seat seat) {
+    public PurchasedTicket purchase(@RequestBody SeatDTO seatDTO) {
+        Seat seat = new Seat(seatDTO.getRow(), seatDTO.getColumn());
         UUID uuid = bookingService.purchaseSeat(seat);
         return new PurchasedTicket(uuid, seat);
     }
 
     @PostMapping("/return")
     public ReturnedTicket returned(@RequestBody Token token) {
-        Seat seat = bookingService.getSeatByToken(token.getToken());
+        Seat seat = bookingService.refundTicket(token.getToken());
         return new ReturnedTicket(seat);
+    }
+
+    @PostMapping("/stats")
+    public Statistic stats(@RequestParam(required = false) String password) {
+        if (password != null && password.equals(secret)) {
+            return statistic;
+        }
+        throw new InvalidPasswordException();
     }
 }
