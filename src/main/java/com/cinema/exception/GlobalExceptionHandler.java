26185a3b5cbbeccec079c27174a277817850b4ca
Денис Векерик <den.vekeryk@gmail.com>
2022-09-06 10:30:23 +0300

    Handle InvalidTokenException.

--- src/main/java/com/cinema/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cinema/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cinema.exception;
 
 import com.cinema.dto.ErrorResponse;
+import com.cinema.exception.custom.InvalidTokenException;
 import com.cinema.exception.custom.SeatAlreadyBookedException;
 import com.cinema.exception.custom.SeatNumberOutOfBoundException;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<?> handleSeatNumberOutOfBoundException(SeatNumberOutOfBoundException exception) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleInvalidTokenException(InvalidTokenException exception) {
         return ResponseEntity.badRequest().body(new ErrorResponse(exception.getMessage()));
     }
 }
