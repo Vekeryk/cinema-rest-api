453b747b75e0ed9b59faa765ff85cc2cb1cb0a26
Денис Векерик <den.vekeryk@gmail.com>
2022-09-06 10:00:32 +0300

    Added price.

--- src/main/java/com/cinema/model/Seat.java
+++ src/main/java/com/cinema/model/Seat.java
@@ -1,12 +1,19 @@
 package com.cinema.model;
 
-import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@EqualsAndHashCode(of = {"row", "column"})
 public class Seat {
 
     private final int row;
     private final int column;
+    private final int price;
+
+    public Seat(int row, int column) {
+        this.row = row;
+        this.column = column;
+        price = row < 5 ? 10 : 8;
+    }
 }
