6194e2b1576085ec28ef22c38f9a5f699493c283
Денис Векерик <den.vekeryk@gmail.com>
2022-09-06 10:49:50 +0300

    Handel InvalidPasswordException.

--- src/main/java/com/cinema/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cinema/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.cinema.exception;
 
 import com.cinema.dto.ErrorResponse;
+import com.cinema.exception.custom.InvalidPasswordException;
 import com.cinema.exception.custom.InvalidTokenException;
 import com.cinema.exception.custom.SeatAlreadyBookedException;
 import com.cinema.exception.custom.SeatNumberOutOfBoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,5 +26,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<?> handleInvalidTokenException(InvalidTokenException exception) {
         return ResponseEntity.badRequest().body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> InvalidPasswordException(InvalidPasswordException exception) {
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 }
