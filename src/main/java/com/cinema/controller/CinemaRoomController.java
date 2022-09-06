ca28ca707a1d6e545246041812f120a504dcd0f4
Денис Векерик <den.vekeryk@gmail.com>
2022-09-06 10:33:43 +0300

    POST /purchase return UUID token. Implement POST /return endpoint, which allow customers to refund their tickets.

--- src/main/java/com/cinema/controller/CinemaRoomController.java
+++ src/main/java/com/cinema/controller/CinemaRoomController.java
@@ -1,25 +1,37 @@
 package com.cinema.controller;
 
+import com.cinema.dto.PurchasedTicket;
+import com.cinema.dto.ReturnedTicket;
+import com.cinema.dto.Token;
 import com.cinema.model.CinemaRoom;
 import com.cinema.model.Seat;
+import com.cinema.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class CinemaRoomController {
 
-    private final CinemaRoom cinemaRoom;
+    private final BookingService bookingService;
 
     @GetMapping("/seats")
     public CinemaRoom seats() {
-        return cinemaRoom;
+        return bookingService.getSeats();
     }
 
     @PostMapping("/purchase")
-    public Seat purchase(@RequestBody Seat seat) {
-        cinemaRoom.purchaseSeat(seat);
-        return seat;
+    public PurchasedTicket purchase(@RequestBody Seat seat) {
+        UUID uuid = bookingService.purchaseSeat(seat);
+        return new PurchasedTicket(uuid, seat);
+    }
+
+    @PostMapping("/return")
+    public ReturnedTicket returned(@RequestBody Token token) {
+        Seat seat = bookingService.getSeatByToken(token.getToken());
+        return new ReturnedTicket(seat);
     }
 }
